b776792e3225a9a7f1ed86cb72efa3ebf158a25e
houscj <housarns@gmail.com>
2021-05-18 07:12:40 +0200

    update

--- app/src/main/java/com/udacity/shoestore/views/shoe_listing/ShoeListingFragment.kt
+++ app/src/main/java/com/udacity/shoestore/views/shoe_listing/ShoeListingFragment.kt
@@ -2,14 +2,13 @@ package com.udacity.shoestore.views.shoe_listing
 
 import android.os.Bundle
 import android.view.*
-import android.widget.TextView
-import androidx.activity.OnBackPressedCallback
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.ShoeItemBinding
 import com.udacity.shoestore.databinding.ShoeListFragmentBinding
 import com.udacity.shoestore.viewmodels.ShoeViewModel
 
@@ -33,19 +32,23 @@ class ShoeListingFragment : Fragment() {
         viewModel.listOfShoes.observe(viewLifecycleOwner, Observer { listShoes ->
             for (shoe in listShoes) {
 
-                val shoeItem = layoutInflater.inflate(R.layout.shoe_item, null)
-                shoeItem.findViewById<TextView>(R.id.shoe_name_list_item).text = shoe.name
+
+                val itemBinding: ShoeItemBinding = DataBindingUtil.inflate(
+                    inflater, R.layout.shoe_item, container, false
+                )
+
+                itemBinding.shoeNameListItem.text = shoe.name
                 val companyName =
                     resources.getString(R.string.company_label_shoe_item) + " " + shoe.company
-                shoeItem.findViewById<TextView>(R.id.company_name_list_item).text = companyName
+                itemBinding.companyNameListItem.text = companyName
                 val size =
                     resources.getString(R.string.size_label_shoe_item) + " " + shoe.size.toString()
-                shoeItem.findViewById<TextView>(R.id.shoe_size_list_item).text = size
+                itemBinding.shoeSizeListItem.text = size
                 val description =
                     resources.getString(R.string.description_label_shoe_item) + " " + shoe.description
-                shoeItem.findViewById<TextView>(R.id.description_list_item).text = description
+                itemBinding.descriptionListItem.text = description
 
-                binding.shoeListLinearLayout.addView(shoeItem)
+                binding.shoeListLinearLayout.addView(itemBinding.root)
             }
         })
 
